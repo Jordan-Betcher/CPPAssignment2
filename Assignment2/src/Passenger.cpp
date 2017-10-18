b639c674ed90a399cc0d25ab1fced958343a4976
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-10-18 09:04:25 -0700

    Generate the methods in Passenger.cpp

---
+++ Assignment2/src/Passenger.cpp
@@ -0,0 +1,84 @@
+/*
+ * Passenger.cpp
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: Betcher
+ */
+
+#include "Passenger.h"
+
+Passenger::Passenger()
+{
+	// TODO Auto-generated constructor stub
+	
+}
+
+bool Passenger::isValidFirstName(std::string allocator)
+{
+}
+
+bool Passenger::isValidLastName(std::string allocator)
+{
+}
+
+bool Passenger::isValidAddress(std::string allocator)
+{
+}
+
+bool Passenger::isValidPhoneNumber(std::string allocator)
+{
+}
+
+void Passenger::setFirstName(std::string allocator)
+{
+}
+
+void Passenger::setLastName(std::string allocator)
+{
+}
+
+void Passenger::setAddress(std::string allocator)
+{
+}
+
+void Passenger::setPhoneNumber(std::string allocator)
+{
+}
+
+void Passenger::getFirstName(std::string allocator)
+{
+}
+
+void Passenger::getLastName(std::string allocator)
+{
+}
+
+void Passenger::getAddress(std::string allocator)
+{
+}
+
+void Passenger::getPhoneNumber(std::string allocator)
+{
+}
+
+bool Passenger::isFirstNameEmpty()
+{
+}
+
+bool Passenger::isLastNameEmpty()
+{
+}
+
+bool Passenger::isAddressEmpty()
+{
+}
+
+bool Passenger::isPhoneNumberEmpty()
+{
+}
+
+Passenger::~Passenger()
+{
+	// TODO Auto-generated destructor stub
+}
+
