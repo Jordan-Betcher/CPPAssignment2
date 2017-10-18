f415a2885ff6e46c40de0b68a2d1a14a3d577028
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-10-18 12:36:41 -0700

    Create OrderedLinkedList

---
+++ Assignment2/src/OrderedLinkedList.cpp
@@ -0,0 +1,20 @@
+/*
+ * OrderedLinkedList.cpp
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: Betcher
+ */
+
+#include "OrderedLinkedList.h"
+
+OrderedLinkedList::OrderedLinkedList()
+{
+	// TODO Auto-generated constructor stub
+	
+}
+
+OrderedLinkedList::~OrderedLinkedList()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Assignment2/src/OrderedLinkedList.h
@@ -0,0 +1,20 @@
+/*
+ * OrderedLinkedList.h
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: Betcher
+ */
+
+#ifndef ORDEREDLINKEDLIST_H_
+#define ORDEREDLINKEDLIST_H_
+
+#include "LinkedList.h"
+
+class OrderedLinkedList: public LinkedList
+{
+	public:
+		OrderedLinkedList();
+		virtual ~OrderedLinkedList();
+};
+
+#endif /* ORDEREDLINKEDLIST_H_ */
