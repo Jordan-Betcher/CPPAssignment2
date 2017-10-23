907f1a6753be77d2e99c481af797013462cd2ebe
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-10-23 11:29:40 -0700

    OrderedLinkedList deleteNode and search

--- Assignment2/src/OrderedLinkedList.h
+++ Assignment2/src/OrderedLinkedList.h
@@ -11,40 +11,94 @@
 #include "LinkedList.h"
 
 template<class T>
-class OrderedLinkedList : public LinkedList<T>
+class OrderedLinkedList: public LinkedList<T>
 {
 	public:
 		OrderedLinkedList();
-
 		void insert(T&);
 		void deleteNode(T&);
 		T& search(T&);
 		~OrderedLinkedList();
+
 };
 
 template<class T>
-inline OrderedLinkedList<T>::OrderedLinkedList()
+OrderedLinkedList<T>::OrderedLinkedList()
 {
+	this->head = NULL;
+	this->last = NULL;
+	this->count = 0;
 }
 
 template<class T>
-inline void OrderedLinkedList<T>::insert(T&)
+void OrderedLinkedList<T>::deleteNode(T& itemToDelete)
 {
+		this->deleteNode(itemToDelete);
 }
 
 template<class T>
-inline void OrderedLinkedList<T>::deleteNode(T&)
+void OrderedLinkedList<T>::insert(T& itemToInsert)
 {
+	node<T> *newNode = new node<T>;
+
+	newNode->data = itemToInsert;
+	newNode->next = NULL;
+
+	if (this->head == NULL)
+	{
+		this->head = newNode;
+		this->last = newNode;
+	}
+	else
+	{
+		this->last->next = newNode;
+		this->last = newNode;
+	}
+
+	this->count++;
 }
 
 template<class T>
-inline T& OrderedLinkedList<T>::search(T&)
+inline T& OrderedLinkedList<T>::search(T& itemToFind)
 {
+	if (head == NULL)
+	{
+
+	}
+	else
+	{
+		if (head->data == itemToFind)
+		{
+			return head->data;
+		}
+		else
+		{
+			node<T>* pointerBefore = head;
+			node<T>* pointerToFind = pointerBefore->next;
+
+			while (pointerToFind != NULL && pointerToFind->data != itemToFind)
+			{
+				pointerBefore = pointerToFind;
+				pointerToFind = pointerToFind->next;
+			}
+
+			if (pointerToFind != NULL)
+			{
+				T found = pointerToFind->data;
+				delete pointerToFind;
+				return found;
+
+			}
+		}
+	}
+
+	throw exception();
 }
 
 template<class T>
-inline OrderedLinkedList<T>::~OrderedLinkedList()
+OrderedLinkedList<T>::~OrderedLinkedList()
 {
+	this->destroyList();
 }
 
 #endif /* ORDEREDLINKEDLIST_H_ */
